6ed8e73d51122487fd423f54d89c5a390a796d7a
Eron Luna <eronluna@yahoo.com>
2019-08-27 09:57:28 -0500

    Init commit

---
+++ src/app.js
@@ -0,0 +1,73 @@
+
+const path = require('path')
+const express = require('express')
+
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+app.use(express.static(publicDirectoryPath))
+
+
+//Setup handlebars engine and views location
+
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//
+app.get('', (req, res) => {
+    res.render('index', {
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send( {
+            error: 'You must provide and address!'
+        })
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+
+    })
+
+
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Eron Luna',
+        errorMessage: 'Page not found.'
+    })
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
+
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/b46edf2763f6d21bb1a21c4d3ba48f7b/' + latitude + ', ' + longitude
+
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently '  + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain ')
+
+        }
+    })
+
+}
+
+module.exports = forecast
